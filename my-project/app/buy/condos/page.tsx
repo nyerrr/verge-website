bd05a29ef3af0ac95200d299ecb6565911e8ec91
nyerrr <160138914+nyerrr@users.noreply.github.com>
2025-11-16 18:05:35 +0800

    pre-selling

---
+++ my-project/app/buy/condos/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Condos() {
+  return (
+    <div className="bg-white min-h-screen relative pt-32">
+      <p className="text-4xl text-black text-center">dadada</p>
+    </div>
+  )
+}
